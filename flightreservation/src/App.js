da69e8a077b301360849043ef2ca2cd4fe4559b6
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-11-28 11:10:45 -0800

    Complete FindFlights component

--- flightreservation/src/App.js
+++ flightreservation/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import FindFlights from './components/FindFlights';
+import DisplayFlights from './components/DisplayFlights';
+import PassengerDetails from './components/PassengerDetails';
+import ConfirmReservation from './components/ConfirmReservation';
 
 function App() {
   return (
     <div className="App">
-      Hello App
+      <Switch>
+        <Route exact path='/' component={FindFlights} />
+        <Route exact path='/displayFlights/:from/:to/:departureDate' component={DisplayFlights} />
+        <Route exact path='/passengerDetails' component={PassengerDetails} />
+        <Route exact path='/confirmReservation' component={ConfirmReservation} />
+      </Switch>
+
     </div>
   );
 }
--- flightreservation/src/components/FindFlights.js
+++ flightreservation/src/components/FindFlights.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class FlindFlights extends React.Component {
+
+  // onChangeHandler(event) {
+  //   console.log('onChangeHandler', event.target.value)
+  // }
+
+  handleSubmit(event) {
+    this.props.history.push('displayFlights/' + '/' + this.from+'/'+this.to+'/'+this.departureDate)
+
+  }
+
   render() {
     return (
-      <div></div>
+      <div>
+        <h2>Find Flights:</h2>
+        <form>
+        From:<input type='text' name='from' onChange={(event)=>{this.from=
+        event.target.value}}/>
+        To:<input type='text' name='to' onChange={(event) => {this.to=event.target.value}}/>
+          {/* From:<input type='text' name='from' onChange={this.onChangeHandler.bind(this)}/>
+          To:<input type='text' name='to' onChange={this.onChangeHandler.bind(this)}/> */}
+          Departure Date:
+          <input type='text' name='departureDate'/>
+          <button onClick={this.handleSubmit.bind(this)} value='search' />
+
+        </form>
+      </div>
     )
   }
 
